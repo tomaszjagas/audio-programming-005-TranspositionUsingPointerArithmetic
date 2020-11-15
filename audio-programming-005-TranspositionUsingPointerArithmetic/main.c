2736c4d8b82ddff6f3c4ef47ceab2d308a8b585b
Tomasz Jagas <tomasz.jagas@gmail.com>
2020-11-15 22:42:17 +0100

    Add project files.

---
+++ audio-programming-005-TranspositionUsingPointerArithmetic/main.c
@@ -0,0 +1,56 @@
+/********************************************************
+Description: Program calculates the result of transposing
+a note by any number of semitones (positive or negative)
+*********************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+// function that keep modulo 12
+int mod12(int note) {
+	while (note < 0) {
+		note += 12;
+	}
+	while (note >= 12) {
+		note -= 12;
+	}
+	return note;
+}
+
+int main() {
+	char note[3], ** p1, ** p2, * table[12] = { "C","C#","D","D#","E","F","F#","G","G#","A","A#","B" };
+	int interval;
+
+	// get input
+	printf("Enter base note (capitals, use # for sharps, eg. A#): ");
+	scanf("%s", note);
+	printf("Enter interval in semitones: ");
+	scanf("%d", &interval);
+
+	// point p1 to the beginning of array and p2 to its end
+	p1 = table;
+	p2 = table + 11;
+
+	// find the base note position, incrementing pointer until it's found
+	while (strcmp(*p1, note)) {
+		p1++;
+		if (p1 > p2) { // if end is passed
+			printf("cound not find %s\n", note);
+			return 1;
+		}
+	}
+	
+	// add the interval to the address of the base note
+	p1 += mod12(interval);
+
+	// if beyond the end of the table, wrap it around
+	if (p1 > p2) {
+		p1 -= 12;
+	}
+
+	// print result
+	printf("%s transposed by %d semitones is %s\n", note, interval, *p1);
+
+	return 0;
+}
+
